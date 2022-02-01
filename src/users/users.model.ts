1d27ae13a5acfb9e5ff38d5c4ea4b8bbf1c706e4
Evgeniy Antipov <evantype@gmail.com>
2022-02-02 00:34:49 +0300

    added user model decorations for swagger

--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,4 +1,5 @@
 import {Column, DataType, Model, Table} from "sequelize-typescript";
+import {ApiProperty} from "@nestjs/swagger";
 
 interface UserCreationAttrs {
     email: string,
@@ -7,18 +8,24 @@ interface UserCreationAttrs {
 
 @Table({tableName: 'users'})
 export class User extends Model<User, UserCreationAttrs> {
+
+    @ApiProperty({example: '1', description: 'id'})
     @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
     id: number;
 
+    @ApiProperty({example: 'evantype@gmail.com', description: 'email'})
     @Column({type: DataType.STRING, unique: true, allowNull: false})
     email: string;
 
+    @ApiProperty({example: 'password', description: 'password'})
     @Column({type: DataType.STRING, allowNull: false})
     password: string;
 
+    @ApiProperty({example: 'true', description: 'is user banned?'})
     @Column({type: DataType.BOOLEAN, defaultValue: false})
     banned: boolean;
 
+    @ApiProperty({example: 'fashion police', description: 'reason for ban'})
     @Column({type: DataType.BOOLEAN, allowNull: true})
     banReason: string;
 }
