d5bb45be37741ce11cd5a81da233dc321e7d3713
Evgeniy Antipov <evantype@gmail.com>
2022-02-01 12:59:35 +0300

    rm bad service and controller and add users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import {Module} from "@nestjs/common";
-import {AppController} from "./app.controller";
-import {AppService} from "./app.service";
 import {SequelizeModule} from "@nestjs/sequelize";
 import { UsersModule } from './users/users.module';
 
 @Module({
-    controllers: [AppController],
-    providers: [AppService],
+    controllers: [],
+    providers: [],
     imports: [
         SequelizeModule.forRoot({
             dialect: 'postgres',
