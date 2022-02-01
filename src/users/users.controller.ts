29ad47580bf83caa3ad244f15fef9ef4192ad70d
Evgeniy Antipov <evantype@gmail.com>
2022-02-02 00:30:39 +0300

    added user controller decorations for swagger

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,8 @@
 import {Body, Controller, Get, Post} from '@nestjs/common';
 import {CreateUserDto} from "./dto/create-user.dto";
 import {UsersService} from "./users.service";
+import {ApiOperation, ApiResponse} from "@nestjs/swagger";
+import {User} from "./users.model";
 
 @Controller('users')
 export class UsersController {
@@ -8,11 +10,15 @@ export class UsersController {
     constructor(private usersService: UsersService) {
     }
 
+    @ApiOperation({summary: 'Create user'})
+    @ApiResponse({status: 200, type: User})
     @Post()
     create(@Body() userDto: CreateUserDto) {
         return this.usersService.createUser(userDto);
     }
 
+    @ApiOperation({summary: 'Get users'})
+    @ApiResponse({status: 200, type: [User]})
     @Get()
     getAll() {
         return this.usersService.getAllUsers();
