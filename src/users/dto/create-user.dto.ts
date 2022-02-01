04d353ef33870c4562ae744fdbc0752fcbd49baa
Evgeniy Antipov <evantype@gmail.com>
2022-02-02 00:37:48 +0300

    added user dto decorations for swagger

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,10 @@
+import {ApiProperty} from "@nestjs/swagger";
+
 export class CreateUserDto {
+
+    @ApiProperty({example: 'evantype@gmail.com', description: 'email'})
     readonly email: string;
+
+    @ApiProperty({example: 'password', description: 'password'})
     readonly password: string;
 }
