02d609380f9be01d9dbb84c0a1b88620e187f838
Evgeniy Antipov <evantype@gmail.com>
2022-02-02 01:49:37 +0300

    user roles

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -1,15 +1,19 @@
-import { Module } from '@nestjs/common';
-import { RolesService } from './roles.service';
-import { RolesController } from './roles.controller';
+import {Module} from '@nestjs/common';
+import {RolesService} from './roles.service';
+import {RolesController} from './roles.controller';
 import {SequelizeModule} from "@nestjs/sequelize";
 import {Role} from "./roles.model";
 import {User} from "../users/users.model";
 
 @Module({
-  providers: [RolesService],
-  controllers: [RolesController],
-  imports: [
-      SequelizeModule.forFeature([Role, User])
-  ]
+    providers: [RolesService],
+    controllers: [RolesController],
+    imports: [
+        SequelizeModule.forFeature([Role, User])
+    ],
+    exports: [
+        RolesService
+    ]
 })
-export class RolesModule {}
+export class RolesModule {
+}
